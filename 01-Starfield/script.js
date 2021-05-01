13480621a60a8c94981c566a00b98604fdab6f4a
Damian Zalewski <damian_zalewskipl@o2.pl>
2021-05-01 12:36:48 +0200

    one particle is moving! YaaY!!

--- 01-Starfield/script.js
+++ 01-Starfield/script.js
@@ -1,27 +1,50 @@
-console.log('test');
 var c = document.getElementById("canvas");
 var ctx = c.getContext("2d");
+
+var centerX = canvas.width / 2;
+var centerY = canvas.height / 2;
 
 let canvasWidth = 800;
 let canvasHeight = 600;
 
+let particles = [];
+particles.push([centerX, centerY]);
 
-init();
+let radius = 10;
+
+clearField();
+setInterval(init, 10);
 
 function init(){
-    // clearField();
-    drawParticles();
+    clearField();
+    for(let i = 0; i<particles.length; i++){
+        drawCircle(radius, particles[i][0], particles[i][1]);
+        particles[i][0]+=i+1;
+        particles[i][1]+=i+1;
+
+        if(particles[i][0] > canvasWidth + radius || particles[i][0] < -radius){
+            particles[i][0] = centerX;
+            particles[i][1] = centerY;
+        }
+
+        if(particles[i][1] > canvasHeight + radius || particles[i][1] < -radius){
+            particles[i][0] = centerX;
+            particles[i][1] = centerY;
+        }
+    }
 }
 
 function clearField(){
-    ctx.fillStyle = "black";
+    ctx.beginPath();
+    ctx.fillStyle = "#000";
     ctx.moveTo(0, 0);
     ctx.rect(0, 0, canvasWidth, canvasHeight);
     ctx.fill();
 }
 
-// function drawParticles(){
-//     context.arc(19, 10, 5, 0, 2 * Math.PI, false);
-//     context.fillStyle = 'green';
-//     context.fill();
-// }
+function drawCircle(radius, x, y){
+    ctx.beginPath();
+    ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+    ctx.fillStyle = '#fff';
+    ctx.fill();
+}
